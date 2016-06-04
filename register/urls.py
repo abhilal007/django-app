7409320cc1329295212038837151bb034a4be8b0
abhilal007 <root@11CPU0197L.amritavidya.edu>
2016-06-04 12:35:59 +0530

    signout

--- register/urls.py
+++ register/urls.py
@@ -4,8 +4,11 @@ from django.contrib import admin
 from register import views
 from register.views import *
 from register.models import *
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', UserRegistrationView.as_view(), name='register_user'),
+    url(r'^user/success/', TemplateView.as_view(template_name='sucess.html'),
+        name='page')
 
 ]
